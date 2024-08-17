c9ca8b25d6869d70901a354b2dd7e2e07181f3fb
afolabi <yusufafolabi95@gmail.com>
2024-08-17 02:23:25 +0100

    updated

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,14 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const secret = 'JWT_SECRET_KEY';
-
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
+    
     if (!token) return res.status(401).json({ error: 'Access denied' });
 
     try {
-        const decoded = jwt.verify(token, secret);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY!);
         (req as any).user = decoded;
         next();
     } catch (err) {
